e6e9b5fefb5085c96e87ae9c1fce3ef06d995e9b
Victor Yamchinov <kodiers@gmail.com>
2018-11-29 04:54:27 +0300

    Initial Commit

---
+++ BrainTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrainTraining
+//
+//  Created by Viktor Yamchinov on 29/11/2018.
+//  Copyright © 2018 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
