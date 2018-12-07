6ea34f48269a1f1ceef768eab63d6edbddb98590
Victor Yamchinov <kodiers@gmail.com>
2018-12-07 03:57:27 +0300

    added image recognition

--- BrainTraining/ViewController.swift
+++ BrainTraining/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Vision
 
 struct Question {
     var text: String
@@ -21,6 +22,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var questions = [Question]()
     var score = 0
+    var digitsModel = Digits()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +36,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func numberDrawn(_ image: UIImage) {
-        askQuestion()
+        let modelSize = 299
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: modelSize, height: modelSize), true, 1.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: modelSize, height: modelSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        guard let ciImage = CIImage(image: newImage) else {
+            fatalError("Failed to convert UIImage to CIImage")
+        }
+        guard let model = try? VNCoreMLModel(for: digitsModel.model) else {
+            fatalError("Failed to prepare model for Vision")
+        }
+        let request = VNCoreMLRequest(model: model) {[weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation], let prediction = results.first else {
+                fatalError("Failed to make prediction \(error?.localizedDescription ?? "Unknown error")")
+            }
+            DispatchQueue.main.async {
+                let result = Int(prediction.identifier) ?? 0
+                self?.questions[0].actual = result
+                if self?.questions[0].correct == result {
+                    self?.score += 1
+                }
+                self?.askQuestion()
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
     
     func setText(for cell: UITableViewCell, at indexPath: IndexPath, to question: Question) {
@@ -77,6 +110,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func askQuestion() {
+        if questions.count == 20 {
+            let ac = UIAlertController(title: "Game over", message: "You scored \(score)/20", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Play again", style: .default, handler: restartGame))
+            present(ac, animated: true, completion: nil)
+            return
+        }
         drawView.image = nil
         questions.insert(createQuestion(), at: 0)
         let newIndexPath = IndexPath(row: 0, section: 0)
@@ -85,6 +124,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         if let cell = tableView.cellForRow(at: secondIndexPath) {
             setText(for: cell, at: secondIndexPath, to: questions[1])
         }
+    }
+    
+    func restartGame(action: UIAlertAction) {
+        score = 0
+        questions.removeAll()
+        tableView.reloadData()
+        askQuestion()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
